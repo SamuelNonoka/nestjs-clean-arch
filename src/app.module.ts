866dcb69d8ff28b0ef712cfa1589cc8afc65c980
Samuel Pereira <nonokapereira@gmail.com>
2024-03-23 16:05:20 -0300

    feat: installing prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
 import { UsersModule } from './users/infrastructure/users.module';
+import { DatabaseModule } from './shared/infrastructure/database/database.module';
 
 @Module({
   imports: [
     EnvConfigModule.forRoot({ isGlobal: true }),
-    UsersModule
+    UsersModule,
+    DatabaseModule
   ],
   controllers: [AppController],
   providers: [
