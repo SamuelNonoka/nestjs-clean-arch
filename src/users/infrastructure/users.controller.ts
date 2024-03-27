e78f53045c1939d8511220e79f40c5dc3ce5d91f
Samuel Pereira <nonokapereira@gmail.com>
2024-03-26 21:02:08 -0300

    feat: implementing users presenter

--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -11,6 +11,8 @@ import { ListUsersUseCase } from '../application/usecases/listuser.usecase';
 import { SigninDto } from './dtos/signin.dto';
 import { ListUsersDto } from './dtos/list-users.dto';
 import { UpdatePasswordDto } from './dtos/update-password.dto';
+import { UserOutput } from '../application/dtos/user-output';
+import { UserCollectionPresenter, UserPresenter } from './presenters/user.presenter';
 
 @Controller('users')
 export class UsersController {
@@ -35,35 +37,49 @@ export class UsersController {
   @Inject(ListUsersUseCase.UseCase)
   private listUsersUseCase: ListUsersUseCase.UseCase
 
+  static userToResponse(output: UserOutput) {
+    return new UserPresenter(output)
+  }
+
+  static listUserToResponse(output: ListUsersUseCase.Output) {
+    return new UserCollectionPresenter(output)
+  }
+
   @Post()
-  create(@Body() signupDto: SignupDto) {
-    return this.signupUseCase.execute(signupDto);
+  async create(@Body() signupDto: SignupDto) {
+    const output = await this.signupUseCase.execute(signupDto);
+    return UsersController.userToResponse(output)
   }
 
   @HttpCode(200)
   @Post('login')
   async login(@Body() signinDto: SigninDto) {
-    return this.signinUseCase.execute(signinDto);
+    const output = await this.signinUseCase.execute(signinDto);
+    return UsersController.userToResponse(output)
   }
 
   @Get()
   async search(@Query() searchParams: ListUsersDto) {
-    return this.listUsersUseCase.execute(searchParams);
+    const output = await this.listUsersUseCase.execute(searchParams);
+    return UsersController.listUserToResponse(output)
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    return this.getUserUseCase.execute({ id });
+    const output = await this.getUserUseCase.execute({ id });
+    return UsersController.userToResponse(output)
   }
 
   @Put('id')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.updateUserUseCase.execute({ id, ...updateUserDto })
+    const output = await this.updateUserUseCase.execute({ id, ...updateUserDto })
+    return UsersController.userToResponse(output)
   }
 
   @Patch('id')
   async updatePassword(@Param('id') id: string, @Body() updatePasswordDto: UpdatePasswordDto) {
-    return this.updatePasswordUseCase.execute({ id, ...updatePasswordDto })
+    const output = await this.updatePasswordUseCase.execute({ id, ...updatePasswordDto })
+    return UsersController.userToResponse(output)
   }
 
   @HttpCode(204)
